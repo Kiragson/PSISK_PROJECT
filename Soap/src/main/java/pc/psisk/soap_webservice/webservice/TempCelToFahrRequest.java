8e7dd58aeb8dee07ac02a79849180a20d094803c
PawChu01 <131011690+PawChu01@users.noreply.github.com>
2024-06-12 21:57:23 +0200

    Update TempCelToFahrRequest.java

--- Soap/src/main/java/pc/psisk/soap_webservice/webservice/TempCelToFahrRequest.java
+++ Soap/src/main/java/pc/psisk/soap_webservice/webservice/TempCelToFahrRequest.java
@@ -1,4 +1,5 @@
-
+//klasa stworzona na podstawie pliku xsd
+//klasa służąca do stworzenia zapytania do zmiany temperatury z stopni Celcjusza na Fahr
 package pc.psisk.soap_webservice.webservice;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
