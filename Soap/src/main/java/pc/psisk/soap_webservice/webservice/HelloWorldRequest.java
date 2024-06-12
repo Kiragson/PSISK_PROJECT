0ba0ebec24a5af087d20895ed530aed74307e8f3
PawChu01 <131011690+PawChu01@users.noreply.github.com>
2024-06-12 21:53:17 +0200

    Update HelloWorldRequest.java

--- Soap/src/main/java/pc/psisk/soap_webservice/webservice/HelloWorldRequest.java
+++ Soap/src/main/java/pc/psisk/soap_webservice/webservice/HelloWorldRequest.java
@@ -1,4 +1,5 @@
-
+//klasa stworzona na podstawie pliku xsd
+//klasa służąca do stworzenia zapytania dla wygenerowania string
 package pc.psisk.soap_webservice.webservice;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
