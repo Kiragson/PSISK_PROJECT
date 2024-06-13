9493922843469be6b57f61397528fbfb3b7f7892
PawChu01 <131011690+PawChu01@users.noreply.github.com>
2024-06-13 17:09:59 +0200

    Update SoapWebserviceApplication.java

--- Soap/src/main/java/pc/psisk/soap_webservice/SoapWebserviceApplication.java
+++ Soap/src/main/java/pc/psisk/soap_webservice/SoapWebserviceApplication.java
@@ -1,3 +1,4 @@
+//główna klasa webservice
 package pc.psisk.soap_webservice;
 
 import org.apache.catalina.connector.Connector;
@@ -10,20 +11,28 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SoapWebserviceApplication {
 
+    //funkcja główna
     public static void main(String[] args) {
         SpringApplication.run(SoapWebserviceApplication.class, args);
     }
 
+    //metoda tworząca i konfigurująca fabrykę serwera webowego
     @Bean
     public ServletWebServerFactory servletContainer() {
+        //tworzenie instancji fabryki serwera Tomcat
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        //dodawanie dodatkowego konektora HTTP do fabryki Tomcat
         tomcat.addAdditionalTomcatConnectors(createHttpConnector());
         return tomcat;
     }
 
+    //metoda tworząca konektor HTTP
     private Connector createHttpConnector() {
+        //tworzenie nowego konektora z domyślnym protokołem Tomcat
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        //ustawienie schemtu na HTTP
         connector.setScheme("http");
+        //dodanie portu
         connector.setPort(8080); // Port HTTP
         return connector;
     }
